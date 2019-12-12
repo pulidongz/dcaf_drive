bf0dcd8d3f1a32a642319574c9f6779f6900277b
pul <paogravoso@gmail.com>
2019-12-12 17:24:54 +0800

    createfile script working

--- dcaf_drive/scripts/scripts.py
+++ dcaf_drive/scripts/scripts.py
@@ -13,18 +13,36 @@ def Ping():
   print("Hello from a function")
 
 def CreateFile():
-	filename_1 = get_random_string(length=10) + '.TIFF'
-	file_1 = open(settings.DATA_DIR+'/landsat/'+filename_1, "w+")
-	for i in range(10):
-		file_1.write("This is line %d\r\n" % (i+1))
-	file_1.close()
-	filename_2 = get_random_string(length=10) + '.txt'
-	file_2 = open(settings.DATA_DIR+'/landsat/'+filename_2, "w+")
-	for i in range(10):
-		file_2.write("This is line %d\r\n" % (i+1))
-	file_2.close()
-	filename_3 = get_random_string(length=10) + '.shp'
-	file_3 = open(settings.DATA_DIR+'/landsat/'+filename_3, "w+")
-	for i in range(10):
-		file_3.write("This is line %d\r\n" % (i+1))
-	file_3.close()
+	filename = get_random_string(length=10)
+	os.mkdir('landsat/'+filename)
+	file_dat = open(settings.DATA_DIR+'/landsat/'+filename+'/'+filename+'.dat', "w+")
+	# for i in range(10):
+	# 	file_1.write("This is line %d\r\n" % (i+1))
+	file_dat.close()
+	
+	file_tiff = open(settings.DATA_DIR+'/landsat/'+filename+'/'+filename+'.tif', "w+")
+	file_tiff.close()
+	
+	file_dbf = open(settings.DATA_DIR+'/landsat/'+filename+'/'+filename+'.dbf', "w+")
+	file_dbf.close()
+
+	file_prj = open(settings.DATA_DIR+'/landsat/'+filename+'/'+filename+'.prj', "w+")
+	file_prj.close()
+
+	file_sbn = open(settings.DATA_DIR+'/landsat/'+filename+'/'+filename+'.sbn', "w+")
+	file_sbn.close()
+
+	file_sbx = open(settings.DATA_DIR+'/landsat/'+filename+'/'+filename+'.sbx', "w+")
+	file_sbx.close()
+
+	file_shp = open(settings.DATA_DIR+'/landsat/'+filename+'/'+filename+'.shp', "w+")
+	file_shp.close()
+
+	file_shx = open(settings.DATA_DIR+'/landsat/'+filename+'/'+filename+'.shx', "w+")
+	file_shx.close()
+
+	file_csv = open(settings.DATA_DIR+'/landsat/'+filename+'/'+filename+'.csv', "w+")
+	file_csv.close()
+
+	file_png = open(settings.DATA_DIR+'/landsat/'+filename+'/'+filename+'.png', "w+")
+	file_png.close()
