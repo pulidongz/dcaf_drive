db714b207dd5415283e33b4c1664e4618578ed15
pul <paogravoso@gmail.com>
2020-01-30 17:39:24 +0800

    added crontab syntax helper

--- dcaf_drive/scripts/tasks.py
+++ dcaf_drive/scripts/tasks.py
@@ -9,6 +9,27 @@ from celery.utils.log import get_task_logger
 
 logger = get_task_logger(__name__)
 
+# ┌───────────── minute (0 - 59)
+# │ ┌───────────── hour (0 - 23)
+# │ │ ┌───────────── day of month (1 - 31)
+# │ │ │ ┌───────────── month (1 - 12)
+# │ │ │ │ ┌───────────── day of week (0 - 6) (Sunday to Saturday;
+# │ │ │ │ │                                       7 is also Sunday on some systems)
+# │ │ │ │ │
+# │ │ │ │ │
+# * * * * *  command_to_execute
+# or check crontab.guru
+
+#run on the 1st and 15th of the month, once @midnight
+#0 0 1,15 * * echo 'Hello' >> /tmp/test.txt
+
+#interval every 10mins
+#/10 * * * * echo...
+
+#range - run every hour from midnight to 5AM
+#0 0-5 * * * echo...
+
+
 # @periodic_task(
 #     run_every=(crontab(minute="*")),
 #     name="add",
